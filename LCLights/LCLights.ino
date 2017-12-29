ef0831222b32d8f35239791c18b62f53ccf039f4
Matthew Slowe <M.Slowe@kent.ac.uk>
2017-12-29 15:39:00 +0000

    working lights

--- LCLights/LCLights.ino
+++ LCLights/LCLights.ino
@@ -1,27 +1,87 @@
-//www.elegoo.com
-//2016.12.08
 
-int ledPin = 5;
-int buttonApin = 9;
-int buttonBpin = 8;
-
-byte leds = 0;
+#define PIN_LED_LEFT 5
+#define PIN_LED_RIGHT 6
+#define PIN_TRIGGER 8
+#define RUN_HEARTBEAT
 
 void setup() 
 {
-  pinMode(ledPin, OUTPUT);
-  pinMode(buttonApin, INPUT_PULLUP);  
-  pinMode(buttonBpin, INPUT_PULLUP);  
+#ifdef RUN_HEARTBEAT
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+#endif
+
+#ifdef DEBUG
+  Serial.begin(9600);
+  Serial.println("\n\n***\nSetting up...");
+#endif
+  pinMode(PIN_LED_LEFT, OUTPUT);
+  pinMode(PIN_LED_RIGHT, OUTPUT);
+  pinMode(PIN_TRIGGER, INPUT_PULLUP);
+
+  digitalWrite(PIN_LED_LEFT, LOW);
+  digitalWrite(PIN_LED_RIGHT, LOW);
+#ifdef DEBUG
+  Serial.println("Setup done");
+#endif
+
+#ifdef RUN_HEARTBEAT
+  digitalWrite(LED_BUILTIN, HIGH);
+#endif
 }
+
+#define DELAY 500 /* ms */
+#define PIN_OFFSET 5
+void runLights(int t) {
+
+#ifdef DEBUG
+  Serial.println("Lights starting");
+#endif
+
+  bool cur = 0;
+  do {
+    digitalWrite(PIN_OFFSET + cur, HIGH);
+    delay(DELAY);
+    digitalWrite(PIN_OFFSET + cur, LOW);
+    t -= DELAY;
+    cur = !cur;
+  } while(t > 0);
+
+#ifdef DEBUG
+  Serial.println("Lights done");
+#endif
+}
+
+#ifdef DEBUG
+int n = 0;
+#endif
+
+#ifdef RUN_HEARTBEAT
+byte looper = 0;
+bool heartbeat_state = HIGH;
+#endif
 
 void loop() 
 {
-  if (digitalRead(buttonApin) == LOW)
-  {
-    digitalWrite(ledPin, HIGH);
+#ifdef RUN_HEARTBEAT
+  if(looper++ == 0) {
+    heartbeat_state = ! heartbeat_state;
+    digitalWrite(LED_BUILTIN, heartbeat_state);
   }
-  if (digitalRead(buttonBpin) == LOW)
+#endif
+
+#ifdef DEBUG
+  Serial.print("Polling trigger n=");
+  Serial.println(n++);
+#endif
+
+  if (digitalRead(PIN_TRIGGER) == LOW)
   {
-    digitalWrite(ledPin, LOW);
+#ifdef DEBUG
+    Serial.println("Running lights...");
+#endif
+    runLights(10 * 1000);
   }
+  delay(5);
+
 }
